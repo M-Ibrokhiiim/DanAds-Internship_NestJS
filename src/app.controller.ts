b06e65fe3fdce0bd29bcc581d81cd686a85e4640
M_Ibrokhiiim <ibrokhimmuysinov@gmail.com>
2026-03-09 19:10:18 +0500

    Made small changes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller('/user/:id')
+@Controller('/user')
 export class AppController {
   constructor(private readonly AppService: AppService) {}
 
