0969d1d3d8a2b7352eae4f53229b0f995c2604f1
M_Ibrokhiiim <ibrokhimmuysinov@gmail.com>
2026-03-10 13:28:01 +0500

    HTTP methods implemented

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -1,10 +1,40 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Delete, Get, Options, Post, Put } from '@nestjs/common';
 
 @Controller('/users')
 export class Users {
 
- @Get()
+  @Get()
   AllUsers():string {
     return `Your order is ready`;
   }
+
+  @Post() 
+   CreateNewUser():string {
+    // 
+    // 
+    // 
+    // 
+    return `User successfully created!`
+
+   }
+
+  @Put()
+    UpdateUser():string {
+      // 
+      // 
+      // 
+      return `User is updated!`
+    }
+
+  @Delete()
+     RemoveUser():string {
+      // 
+      // 
+      return `User deleted!`
+     }
+
+  @Options()
+      Options():string {
+        return ' You can call GET, POST, PUT, DELETE HTTP methods to this users URL!'
+     }
 }
