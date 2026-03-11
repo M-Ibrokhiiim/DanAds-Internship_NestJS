54c64f2a8ee295d4cb64c093d33ce52db55dd6ab
M_Ibrokhiiim <ibrokhimmuysinov@gmail.com>
2026-03-11 15:00:07 +0500

    AppModule rewritten

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { UsersController} from './users/user.controller'
-import { UserService } from './users/user.service';
 import { UsersModule } from './users/user.module';
 
 @Module({
